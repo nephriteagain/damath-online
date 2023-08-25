463595e0109abb3f6976fd58df591940c7fc805c
nephriteagain <oneabove232@gmail.com>
2023-08-25 21:56:43 +0800

    stylings

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -9,16 +9,22 @@ interface ButtonsProps {
 export default function Buttons({showRules}: ButtonsProps) {
     return (
         <div className="absolute top-4 left-4 flex flex-col gap-4">
-            <Button variant="destructive"
-                className="text-md shadow-sm drop-shadow-md hover:scale-105 active:scale-95 transition-all duration-150"
-            >
-                Restart Game
-            </Button>
+           
             <Button variant="default"
                 className="text-md shadow-sm drop-shadow-md hover:scale-105 active:scale-95 transition-all duration-150"
                 onClick={showRules}
             >
                 Show Rules
+            </Button>
+            <Button variant="destructive"
+                className="text-md shadow-sm drop-shadow-md hover:scale-105 active:scale-95 transition-all duration-150"
+            >
+                Restart Game
+            </Button>
+            <Button variant="destructive"
+                className="text-md shadow-sm drop-shadow-md hover:scale-105 active:scale-95 transition-all duration-150"
+            >
+                Leave Game
             </Button>
             <ChangeGameMode/>
 
--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -37,7 +37,7 @@ export default function Piece({piece, index}: PieceProps) {
     const { type, value, movable } = piece
     return (
         <div className={`z-10 ${type === 'z' ? 'red-piece' : 'blue-piece'}
-            ${movable ? 'opacity-100' : 'opacity-50'}
+            ${movable ? 'opacity-100' : 'opacity-70'}
             flex items-center justify-center aspect-square w-[80%] text-2xl text-white rounded-full cursor-pointer`}
             onClick={onClick}
         >
